67fc49fc5e2aa063227205b22e76c424d0d1cd97
Aleksey Verkholantsev <alex-vee@yandex-team.ru>
2014-07-03 15:45:07 +0400

    index.js added

--- test/overload.js
+++ test/overload.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var expect = require('must');
-var overload = require('../lib/overload').overload;
+var overload = require('../lib/overload');
 
 describe('overload', function () {
     it('должен возвращать функцию', function () {
