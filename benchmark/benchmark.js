0a321691b40a7db621dfb4e7b12fe90b11dae185
Aleksei Verkholantcev <averkholantcev@atlassian.com>
2018-02-18 22:37:03 +1100

    Add arguments to benchmarks

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,5 +1,6 @@
 import Benchmark from 'benchmark';
 import range from 'lodash/range';
+import constant from 'lodash/constant';
 
 import produceOverloadedFn from './produce-overloaded-fn';
 import log from './log';
@@ -19,8 +20,10 @@ const fn = () => {
 const overloaded0 = produceOverloadedFn(0, fn);
 
 const overloaded100 = produceOverloadedFn(100, fn);
+const overloaded100Args = range(100).map(constant(1));
 
 const overloaded1000 = produceOverloadedFn(1000, fn);
+const overloaded1000Args = range(1000).map(constant(1));
 
 const suite = new Benchmark.Suite();
 
@@ -29,10 +32,10 @@ suite
         fn();
     })
     .add('Overloaded with 1000x options'.padEnd(40, '.'), () => {
-        overloaded1000();
+        overloaded1000.apply(null, overloaded1000Args);
     })
     .add('Overloaded with 100x options'.padEnd(40, '.'), () => {
-        overloaded100();
+        overloaded100.apply(null, overloaded100Args);
     })
     .add('Overloaded with 0x option'.padEnd(40, '.'), () => {
         overloaded0();
